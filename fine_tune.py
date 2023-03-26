62f00dc3e4cefb07409295c09cc94cf491228f3d
PietroVitiello <vitiellocollegiosancarlo@gmail.com>
2023-03-27 00:49:15 +0100

    [TORCH] Fixed multi rank wandb and ckpt logger

--- fine_tune.py
+++ fine_tune.py
@@ -149,10 +149,12 @@ def main():
 
     # Callbacks
     # TODO: update ModelCheckpoint to monitor multiple metrics
-    ckpt_callback = ModelCheckpoint(monitor='auc@10', verbose=True, save_top_k=5, mode='max',
+    ckpt_callback = ModelCheckpoint(monitor='auc@10', verbose=True, save_top_k=3, mode='max',
                                     save_last=True,
+                                    every_n_train_steps=50,
                                     dirpath=str(ckpt_dir),
                                     filename='{epoch}-{auc@5:.3f}-{auc@10:.3f}-{auc@20:.3f}')
+
     lr_monitor = LearningRateMonitor(logging_interval='step')
     callbacks = [lr_monitor]
     if not args.disable_ckpt:
