4b4393c21eb90ed5c83bf43fd8f2c931ce2cc92d
PietroVitiello <vitiellocollegiosancarlo@gmail.com>
2023-03-27 01:12:20 +0100

    [TORCH] Fixed rank issue

--- fine_tuning/lightning_model.py
+++ fine_tuning/lightning_model.py
@@ -61,7 +61,7 @@ class PL_ASpanFormer(pl.LightningModule):
         self.dump_dir = dump_dir
 
         self.use_wandb = use_wandb
-        if self.trainer.global_rank == 0 and use_wandb:
+        if use_wandb:
             entity = "head-dome"
             print(f'Connecting with {entity} on wandb')
             wandb.init(
