e45d6c15311d59451502911ddebd0f58baf3a95f
pita <vitiellocollegiosancarlo@gmail.com>
2023-05-17 17:44:59 +0100

    [DATA] Modifications for 4dof data

--- fine_tuning/globals.py
+++ fine_tuning/globals.py
@@ -14,4 +14,5 @@ if socket.gethostname() == 'pit-lab':
 elif socket.gethostname() == 'p-pit17':
     DATASET_DIR = Path("/home/pita/Documents/PhD/Aspanformer/clean_dataset")
 else:
-    DATASET_DIR = Path("/rds/general/user/pv2017/projects/head-cam-dome/live/datasets/full_blender_dataset_so")
+    # DATASET_DIR = Path("/rds/general/user/pv2017/projects/head-cam-dome/live/datasets/full_blender_dataset_so")
+    DATASET_DIR = Path("/rds/general/user/pv2017/projects/head-cam-dome/live/datasets/full_sar")
